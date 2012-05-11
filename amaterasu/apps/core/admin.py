9f5e9bf98ce23a57c485d7355c00f4f2cb265ab3
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-05-10 21:46:49 -0600

    Activate the admin interface and adding the core models to admin app

---
+++ amaterasu/apps/core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from models import Mailbox, Alias, Domain
+
+class MailboxAdmin(admin.ModelAdmin):
+    list_display = ('username', 'name', 'created', 'modified', 'active')
+    list_filter = ('created', 'active')
+    search_fields = ['username', 'domain']
+    
+class AliasAdmin(admin.ModelAdmin):
+    list_display = ('address', 'goto')
+
+admin.site.register(Mailbox, MailboxAdmin)
+admin.site.register(Alias, AliasAdmin)
+admin.site.register(Domain)
--- amaterasu/apps/core/models.py
+++ amaterasu/apps/core/models.py
@@ -11,10 +11,10 @@ class Mailbox(models.Model):
     username = models.EmailField(max_length=150)
     password = models.CharField(max_length=250)
     name = models.CharField(max_length=250)
-    maildir = models.CharField(max_length=150)
+    maildir = models.CharField(max_length=150, blank=True)
     quota = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    local_part = models.CharField(max_length=100)
-    domain = models.CharField(max_length=100)
+    local_part = models.CharField(max_length=100, blank=True)
+    domain = models.CharField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
@@ -28,6 +28,9 @@ class Mailbox(models.Model):
     def __unicode__(self):
         return "%s" % self.username
         
+    class Meta:
+        verbose_name_plural = 'Mailboxes'
+        
 class Alias(models.Model):
     address = models.EmailField(max_length=150)
     goto = models.EmailField(max_length=150)
@@ -38,6 +41,9 @@ class Alias(models.Model):
     
     def __unicode__(self):
         return "%s" % self.address
+        
+    class Meta:
+        verbose_name_plural = 'Aliases'
         
 class Domain(models.Model):
     name = models.CharField(max_length=100)
