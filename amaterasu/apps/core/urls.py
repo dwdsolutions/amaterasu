1e39cc3e9df437ce6afe7bf981ba22b4eaf43e94
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-06-02 16:16:05 -0600

    Fix urls for amaterasu

--- amaterasu/apps/core/urls.py
+++ amaterasu/apps/core/urls.py
@@ -4,6 +4,5 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'amaterasu.views.home', name='home'),
-    url(r'^amaterasu/', include('core.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
