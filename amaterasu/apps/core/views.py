e3dbdb26ee3aab8e913142e9faf6d7c4d3add9b7
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-06-18 22:41:11 -0600

    Added new url for index and index view

--- amaterasu/apps/core/views.py
+++ amaterasu/apps/core/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+import uuid
+import logging
+import datetime
+from django.views.generic import FormView, TemplateView, ListView, DetailView, UpdateView, RedirectView
+from django.views.generic.base import View
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
+from pprint import pprint
+
+class IndexView(TemplateView):
+    template_name = "index.html"
