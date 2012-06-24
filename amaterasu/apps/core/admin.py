00ce27cbc26202de6bd31e3fc237ae0b925a309d
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-06-24 15:36:10 -0600

    Added format to add plans on admin

--- amaterasu/apps/core/admin.py
+++ amaterasu/apps/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Mailbox, Alias, Domain, PdnsDomains, Records, ClientProfile
+from models import Mailbox, Alias, Domain, PdnsDomains, Records, Language, Plan, ClientProfile
 
 class MailboxAdmin(admin.ModelAdmin):
     list_display = ('username', 'name', 'created', 'modified', 'active')
@@ -15,6 +15,17 @@ class RecordsAdmin(admin.ModelAdmin):
     search_fields = ['name', 'type', 'content']
     list_per_page = 25
     
+class PlanAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Nombre Plan', {'fields': ['nombre']}),
+        ('Espacios', {'fields': ['espacio', 'transferencia', 'cuentas_correo', 'subdominios', 'cuentas_ftp', 'db_mysql', 'db_postgres']}),
+        ('Lenguajes', {'fields': ['lenguajes']}),
+        ('Precio', {'fields': ['precio']}),
+    ]
+    #inlines = [AdicionalInline]
+    list_display = ('nombre', 'espacio', 'transferencia', 'db_mysql', 'db_postgres', 'precio')
+
+    
 class ClientProfileAdmin(admin.ModelAdmin):
     list_displat = ('get_full_name', 'phone', 'email')
 
@@ -23,5 +34,7 @@ admin.site.register(Alias, AliasAdmin)
 admin.site.register(Domain)
 admin.site.register(PdnsDomains)
 admin.site.register(Records, RecordsAdmin)
+admin.site.register(Language)
+admin.site.register(Plan, PlanAdmin)
 admin.site.register(ClientProfile, ClientProfileAdmin)
 
