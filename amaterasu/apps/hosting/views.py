cbcfe28010c464d15b044e0ce8904974546f2aa5
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-08-18 23:50:55 -0600

    Added new app to manage and add new functionalities to hosting front end

---
+++ amaterasu/apps/hosting/views.py
@@ -0,0 +1,26 @@
+from django.views.generic import FormView, TemplateView, ListView, DetailView, UpdateView, RedirectView, UpdateView
+from django.views.generic.base import View
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from pprint import pprint
+
+class HostingIndexView(ListView):
+    model = Plan
+    template_name = "plan_list.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(HostingView, self).get_context_data(**kwargs)
+        context['hosting_active'] = True
+        
+        return context
+        
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(HostingView, self).dispatch(*args, **kwargs)
