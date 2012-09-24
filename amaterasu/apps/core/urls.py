0047cd39908efa78ba2e1acf7c3e04f94c5a251e
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-09-24 00:36:03 -0600

    Added DomainEditView url for hosting

--- amaterasu/apps/core/urls.py
+++ amaterasu/apps/core/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from views import IndexView, RedirectToIndexView, ProfileView
+from views import IndexView, RedirectToIndexView, ProfileView, DomainEditView
 
 admin.autodiscover()
 
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^login/$', auth_views.login, {'template_name': 'auth/login.html'}, name="auth_login"),
     url(r'^logout/$', RedirectToIndexView.as_view(), name="auth_logout"),
     url(r'^hosting/', include('hosting.urls')),
+    url(r'^/domain/(?P<pk>\d+)/edit/$', DomainEditView.as_view(), name="edit-domain"),
     url(r'^profile/(?P<pk>\d+)/$', ProfileView.as_view(), name="profile"),
     url(r'^admin/', include(admin.site.urls)),
 )
--- amaterasu/apps/hosting/urls.py
+++ amaterasu/apps/hosting/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from views import HostingIndexView, DomainEditView
+from views import HostingIndexView
 
 urlpatterns = patterns('',
     url(r'^$', HostingIndexView.as_view(), name="hosting-index"),
-    url(r'^/domain/(?P<pk>\d+)/edit/$', DomainEditView.as_view(), name="edit-domain"),
+    
 )
