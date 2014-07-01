d5273bc28049c6362d54ff5023abf9f72776961a
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-07-01 17:42:18 -0600

    Fix tuple

--- amaterasu/apps/core/forms.py
+++ amaterasu/apps/core/forms.py
@@ -7,39 +7,39 @@ class SearchForm(forms.Form):
     Form to search and download a file
     """
     search = forms.CharField(widget=forms.TextInput(attrs={'class': 'input-v', 'placeholder': 'C\'mon, give it a try and enter your link here...'}), label="")
-    
+
 class ProfileForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         exclude = ('is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined', 'groups', 'user_permissions')
-        
+
 class ClientProfileForm(forms.ModelForm):
     class Meta:
         model = ClientProfile
-        exclude = ('user')
-        
+        exclude = ('user',)
+
 class DomainForm(forms.ModelForm):
     class Meta:
         model = Domain
         exclude = ('client',)
-        
+
 class MailboxForm(forms.ModelForm):
     class Meta:
         model = Mailbox
         exclude = ('domain',)
-        
+
 class RecordForm(forms.ModelForm):
     class Meta:
         model = Records
         exclude = ('domain',)
-        
+
 class SelectDomainForm(forms.Form):
     domain = forms.ChoiceField(label="Select a Domain", choices=[])
-    
+
     def __init__(self, *args, **kwargs):
         super(SelectDomainForm, self).__init__(*args, **kwargs)
         domains = Domain.objects.filter(active=True)
         domains_choices = []
         for d in domains:
             domains_choices.append((d.id, d.name))
-        
+
         self.fields['domain'].choices = domains_choices
